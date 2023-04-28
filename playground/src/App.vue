78e65c1fde2839da5d7132dfb51ac7d70bca59fd
lijiapeng001 <1146417576@qq.com>
2023-04-28 10:52:28 +0800

    feat: update copyText

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,14 +1,12 @@
-<script lang='ts' setup>
-import { copyText } from '../../src/index'
+<script lang="ts" setup>
+import { copyText } from "../../src/index";
 
 async function upload() {
-  const data = await copyText('我永远喜欢新垣结衣')
-  console.log(data, 'data')
+  const data = await copyText("1651513676538318848");
+  console.log(data, "data");
 }
 </script>
 
 <template>
-  <button @click="upload">
-    upload
-  </button>
+  <button @click="upload">upload</button>
 </template>
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-export function getStyle(element: HTMLElement, property: keyof CSSStyleDeclaration): string {
-  return window.getComputedStyle(element, null)[property] as string
+export function getStyle(
+  element: HTMLElement,
+  property: keyof CSSStyleDeclaration
+): string {
+  return window.getComputedStyle(element, null)[property] as string;
 }
 
 export async function copyText(text: string): Promise<boolean> {
-  const textArea = document.createElement('textarea')
-  const style = {
-    position: 'fixed',
-    top: '-900px',
-    left: '-900px',
-    width: '2em',
-    height: '2em',
-  }
-  Object.assign(textArea.style, style)
-  textArea.value = text
-  document.body.appendChild(textArea)
-  textArea.select()
   try {
-    await navigator.clipboard.writeText(text)
-    document.body.removeChild(textArea)
-    return true
-  }
-  catch (error) {
-    document.body.removeChild(textArea)
-    return false
+    await navigator.clipboard.writeText(text);
+    return true;
+  } catch (error) {
+    const textArea = document.createElement("textarea");
+    const style = {
+      position: "fixed",
+      top: "-900px",
+      left: "-900px",
+      width: "2em",
+      height: "2em",
+    };
+    Object.assign(textArea.style, style);
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    if (document.execCommand) {
+      document.execCommand(text);
+      document.body.removeChild(textArea);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
