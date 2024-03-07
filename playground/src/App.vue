a3b7ba5d2cd1feeee7a1d8e6a3322adda89e9035
lijiapeng001 <1146417576@qq.com>
2024-03-07 17:31:26 +0800

    fix: 优化缓存策略

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,28 +1,41 @@
 <script lang="ts" setup>
 import { createAuthority } from "../../src/authority";
 
-type Test1 = Number[]
+type Test1 = {
+  num?: Number[];
+  name?: string;
+};
 
 let useTest = createAuthority<Test1>({
-  localKey:'test'
-})
-function save(e)
-{
-  useTest.set([Math.random()])
-  let num = useTest.get()
-  console.log(num)
+  localKey: "test",
+  maxAge: 1000 * 5,
+  defaultData: {
+    name: "哈哈哈",
+    num: [22, 33, 44],
+  },
+});
+function save(e) {
+  useTest.set({
+    num: [1, 2, 3],
+    name: "22",
+  });
+  console.log(useTest.get(), "dd");
+}
+function getLocal() {
+  console.log(useTest.get())
 }
 </script>
 
 <template>
   <button @click="save">保存</button>
+  <button @click="getLocal">获取</button>
 </template>
 
 <style>
 body {
   height: 500vh;
 }
-input{
+input {
   width: 200px;
   height: 40px;
   border: 1px solid #000;
--- src/authority.ts
+++ src/authority.ts
@@ -1,70 +1,74 @@
 export interface AuthorityProps<T> {
-  localKey: string
-  localType?: Storage
-  maxAge?: number
-  defaultData?:T
+  localKey: string;
+  localType?: Storage;
+  maxAge?: number;
+  defaultData?: T;
 }
 
-export class Authority<T extends Record<string,any>> {
+export class Authority<T extends Record<string, any>> {
   constructor(private props: Required<AuthorityProps<T>>) {
-    if(props.defaultData) this.set(props.defaultData)
+    if (props.defaultData) {
+      let localD = this.get() as T;
+      Object.keys(props.defaultData).forEach((key: keyof T) => {
+        if (!(key in localD)) this.set({ [key]: props.defaultData[key] } as T);
+      });
+    }
   }
 
   /**
    * 判断数据是否过期
    */
-  private isExpired(data: T): boolean {
-    const { maxAge } = this.props
-    return data.maxAge && Date.now() - data.maxAge >= maxAge!
+  private isExpired(): boolean {
+    return Date.now() >= this.props.maxAge;
   }
 
   /**
    * 获取旧数据
    */
   private getOldData(): T {
-    const { localKey, localType } = this.props
-    return JSON.parse(localType.getItem(localKey!) ?? '{}')
+    const { localKey, localType } = this.props;
+    return JSON.parse(localType.getItem(localKey!) ?? "{}");
   }
 
   /**
    * 设置新数据
    */
   private setData(value: T, oldData: T): void {
-    const { maxAge, localKey, localType } = this.props
+    const { maxAge, localKey, localType } = this.props;
     const data = {
-      [localKey]: value,
+      ...oldData,
+      ...value,
       maxAge: oldData.maxAge || Date.now() + maxAge!,
-    }
-    localType.setItem(localKey!, JSON.stringify(data))
+    };
+    localType.setItem(localKey!, JSON.stringify(data));
   }
 
   /**
    * 获取数据
    */
-  get():T {
-    const {localKey } = this.props
-    const data = this.getOldData()[localKey]
-    if (this.isExpired(data)) {
-      this.clear()
-      return this.props.defaultData ?? {} as T;
+  get(): T {
+    const data = this.getOldData();
+    if (this.isExpired()) {
+      this.clear();
+      return this.props.defaultData ?? ({} as T);
     }
-    return data
+    return data;
   }
 
   /**
    * 设置数据
    */
-  set(value:T) {
-    const oldData = this.getOldData()
-    this.setData(value, oldData)
+  set(value: T) {
+    const oldData = this.getOldData();
+    this.setData(value, oldData);
   }
 
   /**
    * 清除数据
    */
   clear() {
-    const { localKey, localType } = this.props
-    localType.removeItem(localKey)
+    const { localKey, localType } = this.props;
+    localType.removeItem(localKey);
   }
 }
 
@@ -75,9 +79,13 @@ export class Authority<T extends Record<string,any>> {
  * @param {number} props.maxAge 缓存过期时间，默认为 30 天
  * @param {T} props.defaultData 默认数据
  */
-export function createAuthority<T extends Record<string, any>>(props: AuthorityProps<T>) {
+export function createAuthority<T extends Record<string, any>>(
+  props: AuthorityProps<T>
+) {
   const localType = props.localType ?? localStorage;
-  const maxAge = props.maxAge ?? 30 * 1000 * 60 * 60 * 24;
-  const defaultData = props.defaultData ?? {} as T;
+  const defaultData = props.defaultData ?? ({} as T);
+  const maxAge = props.maxAge
+    ? Date.now() + props.maxAge
+    : Date.now() + 30 * 1000 * 60 * 60 * 24;
   return new Authority<T>({ ...props, localType, maxAge, defaultData });
 }
